5ffa30da5ed3d0992b858c9353d11b49ab099355
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-08-31 20:10:45 +0200

    Add files via upload

---
+++ Py-UploadFlaskFiles.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'bmp', 'mp4', 'mp3'}
+
+# Vytvorte priečinok 'uploads' ak neexistuje
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # Skontrolujte, či bol nahratý súbor
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # Uloženie súboru
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return redirect(url_for('upload_file'))
+
+    # Získanie zoznamu nahratých súborov
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', files=files)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/delete/<filename>', methods=['POST'])
+def delete_file(filename):
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    return redirect(url_for('upload_file'))
+
+if __name__ == "__main__":
+    app.run(host='192.168.1.4', port=5000, debug=True)
