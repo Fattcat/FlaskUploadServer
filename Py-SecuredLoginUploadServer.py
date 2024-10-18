0c83d94441d58c10b14d71409f8cc14286947db1
Fattcat - but not fat cat <109801079+Fattcat@users.noreply.github.com>
2024-10-18 22:11:08 +0200

    Update Py-SecuredLoginUploadServer.py

--- Py-SecuredLoginUploadServer.py
+++ Py-SecuredLoginUploadServer.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, send_from_directory, redirect, url_for, session, render_template_string
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for, session, render_template_string, flash
 import os
+from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = 'tajne-heslo'  # Bezpečnostný kľúč pre relácie (sessions)
@@ -44,6 +45,16 @@ html_content_index = """
 </html>
 """
 
+# Dekorátor na kontrolu prihlásenia
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:  # Kontrola, či je používateľ prihlásený
+            flash('Prosím, prihláste sa najprv.')
+            return redirect(url_for('home'))  # Presmerovanie na login stránku, ak nie je prihlásený
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/')
 def home():
     if 'logged_in' in session:
@@ -67,11 +78,10 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('home'))
 
+# Pridanie dekorátora na ochranu stránky
 @app.route('/upload', methods=['GET', 'POST'])
+@login_required  # Ochrana pomocou login_required dekorátora
 def upload_file():
-    if 'logged_in' not in session:
-        return redirect(url_for('home'))
-    
     if request.method == 'POST':
         files = request.files.getlist('file')  # Získanie všetkých nahraných súborov
         for file in files:
@@ -87,16 +97,13 @@ def upload_file():
     return render_template('index.html', files=files)
 
 @app.route('/uploads/<filename>')
+@login_required  # Ochrana pomocou login_required dekorátora
 def uploaded_file(filename):
-    if 'logged_in' not in session:
-        return redirect(url_for('home'))
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/delete/<filename>', methods=['POST'])
+@login_required  # Ochrana pomocou login_required dekorátora
 def delete_file(filename):
-    if 'logged_in' not in session:
-        return redirect(url_for('home'))
-    
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if os.path.exists(file_path):
         os.remove(file_path)
